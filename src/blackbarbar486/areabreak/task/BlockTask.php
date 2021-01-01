f98756a25837d63bc4d201a44e13ba803857c283
blackbarbar <63960767+blackbarbar@users.noreply.github.com>
2021-01-01 17:07:02 +0100

    Update BlockTask.php

--- src/blackbarbar486/areabreak/task/BlockTask.php
+++ src/blackbarbar486/areabreak/task/BlockTask.php
@@ -15,7 +15,6 @@ class BlockTask extends Task {
 	private $pos;
 	private $level;
 	private $plugin;
-	public $seconds = 30;
 	
 	public function __construct(Plugin $plugin, Vector3 $pos, Level $level) {
 		$this->plugin = $plugin;
@@ -24,20 +23,17 @@ class BlockTask extends Task {
 	}
 	
 	public function onRun(int $currentTick) {
-		$this->seconds--;
-		if($this->seconds === 0) {
-			$level = $this->level;
-			$levelname = $level->getName();
-			$x = $this->pos->getX();
-			$y = $this->pos->getY();
-			$z = $this->pos->getZ();
-			$coords = "$x $y $z $levelname";
-			$block = $this->plugin->blocks->get($coords);
-			$block = explode(" ", $block);
-			$blockid = intval($block[1]);
-			$blockmeta = intval($block[2]);
+		$level = $this->level;
+		$levelname = $level->getName();
+		$x = $this->pos->getX();
+		$y = $this->pos->getY();
+		$z = $this->pos->getZ();
+		$coords = "$x $y $z $levelname";
+		$block = $this->plugin->blocks->get($coords);
+		$block = explode(" ", $block);
+		$blockid = intval($block[1]);
+		$blockmeta = intval($block[2]);
 			
-			$level->setBlock(new Vector3($x, $y, $z), Block::get($blockid, $blockmeta));
-		}
+		$level->setBlock(new Vector3($x, $y, $z), Block::get($blockid, $blockmeta));
 	}
 }
